2bf763ecdc268d7ca27eb21ed544828ee076af96
Telicia Watson <teli203@aol.com>
2020-09-05 14:58:40 -0500

    getIdElement to JS

---
+++ js script.js
@@ -0,0 +1,7 @@
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const qImg = document.getElementById("questionImage");
+const question = document.getElementById("question");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+
