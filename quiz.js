ce0173e3c44959ff5bb5bbb29d246bee5f5eceae
Telicia Watson <teli203@aol.com>
2020-09-10 21:43:26 -0500

    adding variables to quiz js

---
+++ quiz.js
@@ -0,0 +1,45 @@
+//Select all Elements//
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const qImg = document.getElementById("questionImage");
+const question = document.getElementById("question");
+const counter = document.getElementById("counter");
+const timeGauge = document.getElementById("timeGauge");
+
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+
+const progress = document.getElementById("progress");
+
+const scorecontainer = document.getElementById("scorecontainer");
+
+//create our questions//
+let questions = [
+    {
+        question : "What does HTML stand for?",
+        imgSrc : "img/html.png",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
+    },{
+        question : "What does CSS stand for?",
+        imgSrc : "img/css.png",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B"
+    },{
+        question : "What does JS stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
+    }
+];
+//Variables//
+const lastQuestion = question.length - 1;
+let runningQuestion = 0;
+let count = 0;
